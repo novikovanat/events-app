7e8e22018e46fc5d5ba032cb385cad819e2f153c
novikovanat <72397136+novikovanat@users.noreply.github.com>
2024-09-22 22:50:16 +0200

    structure draft

---
+++ src/Components/App/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from "react";
+import "./App.css";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <header></header>
+      <main>
+        <p>This is App</p>
+      </main>
+    </>
+  );
+}
+
+export default App;

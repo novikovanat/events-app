065665e5314a8852947ee4fde77e326baef027b9
novikovanat <72397136+novikovanat@users.noreply.github.com>
2024-09-22 23:40:54 +0200

    routing added

--- src/Components/App/App.jsx
+++ src/Components/App/App.jsx
@@ -1,13 +1,43 @@
 import { useState } from "react";
+import { Route, Routes } from "react-router-dom";
+import { Suspense, lazy } from "react";
 import "./App.css";
+import NavigationComponent from "../NavigationComponent/NavigationComponent";
+const EventsPage = lazy(() => import("../../Pages/EventsPage/EventsPage"));
+const SingleEventPage = lazy(() =>
+  import("../../Pages/SingleEventPage/SingleEventPage")
+);
+const ParticipantsComponent = lazy(() =>
+  import("../../Components/ParticipantsComponent/ParticipantsComponent")
+);
+const EventRegistrationComponent = lazy(() =>
+  import("../EventRegistrationComponent/EventRegistrationComponent")
+);
+// const NotFoundPage = lazy(()=>lazy('../../Pages/NotFoundPage/NotFoundPage.jsx'))
+import NotFoundPage from "../../Pages/NotFoundPage/NotFoundPage"
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
     <>
-      <header></header>
+      <header>
+        <NavigationComponent />
+      </header>
       <main>
+        <Suspense fallback="Page loading...">
+          <Routes>
+            <Route path="/events" element={<EventsPage />} />
+            <Route path="/events/:eventId" element={<SingleEventPage />}>
+              <Route path="participants" element={<ParticipantsComponent />} />
+              <Route
+                path="reistration"
+                element={<EventRegistrationComponent />}
+              />
+            </Route>
+            <Route path="*" element={<NotFoundPage />} />
+          </Routes>
+        </Suspense>
         <p>This is App</p>
       </main>
     </>
---
+++ src/Components/NavigationComponent/NavigationComponent.jsx
@@ -0,0 +1,9 @@
+import { Link } from "react-router-dom";
+
+export default function NavigationComponent() {
+  return (
+    <nav>
+      <Link to="/events">Events</Link>
+    </nav>
+  );
+}

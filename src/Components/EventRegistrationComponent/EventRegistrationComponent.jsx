1447a729b21434116ee2b400a8b498c06b566eb9
novikovanat <72397136+novikovanat@users.noreply.github.com>
2024-09-24 15:16:36 +0200

    validation added

--- src/Components/EventRegistrationComponent/EventRegistrationComponent.jsx
+++ src/Components/EventRegistrationComponent/EventRegistrationComponent.jsx
@@ -1,14 +1,35 @@
 import { Formik, Form, Field, ErrorMessage } from "formik";
+import * as Yup from "yup";
 
 export default function EventRegistrationComponent() {
+  const validationSchema = Yup.object().shape({
+    fullName: Yup.string()
+      .min(3, "Too Short!")
+      .max(18, "Too Long!")
+      .required("Name is required!"),
+    Email: Yup.string().email().required("Email is required!"),
+    dateOfBirth: Yup.date("Invalid date format!").required(
+      "Date of birth is required!"
+    ),
+    source: Yup.string().required("Choose option"),
+  });
+  const handleSubmit = (participant, actions) => {
+    console.log("====================================");
+    console.log(participant);
+    console.log("====================================");
+    actions.resetForm();
+  };
+
   return (
     <Formik
       initialValues={{
-        name: "",
-        phoneNumber: "",
+        fullName: "",
+        Email: "",
+        dateOfBirth: "",
+        source: "",
       }}
-      // onSubmit={handleSubmit}
-      // validationSchema={validationSchema}
+      onSubmit={handleSubmit}
+      validationSchema={validationSchema}
     >
       <Form>
         <label>
@@ -28,18 +49,21 @@ export default function EventRegistrationComponent() {
           <Field name="dateOfBirth" />
           <ErrorMessage name="dateOfBirth" component="span" />
         </label>
-
         <label>
-          <Field type="radio" name="source" value="social media" />
-          Social media
-        </label>
-        <label>
-          <Field type="radio" name="source" value="friends" />
-          Friends
-        </label>
-        <label>
-          <Field type="radio" name="source" value="found myself" />
-          Found myself
+          Where did you here about this event?
+          <label>
+            Social media
+            <Field type="radio" name="source" value="social media" />
+          </label>
+          <label>
+            Friends
+            <Field type="radio" name="source" value="friends" />
+          </label>
+          <label>
+            Found myself
+            <Field type="radio" name="source" value="found myself" />
+          </label>
+          <ErrorMessage name="source" component="span" />
         </label>
 
         <button type="submit">Register</button>

e34404d22eda36554ca245737607ae6c65afb090
novikovanat <72397136+novikovanat@users.noreply.github.com>
2024-09-24 22:30:51 +0200

    form changes

--- src/Pages/EventRegistrationPage/EventRegistrationPage.jsx
+++ src/Pages/EventRegistrationPage/EventRegistrationPage.jsx
@@ -2,9 +2,10 @@ import { Formik, Form, Field, ErrorMessage } from "formik";
 import * as Yup from "yup";
 import { postParticipant } from "../../js/fetch";
 import { useParams } from "react-router-dom";
-
+import { useState } from "react";
 export default function EventRegistrationComponent() {
   const { eventId } = useParams();
+  const [loading, setLoading] = useState(false);
 
   const validationSchema = Yup.object().shape({
     fullName: Yup.string()
@@ -19,62 +20,65 @@ export default function EventRegistrationComponent() {
   });
   const handleSubmit = async (participant, actions) => {
     try {
+      setLoading(true);
       const newParticipant = await postParticipant(eventId, participant);
       return newParticipant;
     } catch (error) {
-      console.log(error.ErrorMessage);
+      alert(error.message);
+    } finally {
+      setLoading(false);
       actions.resetForm();
     }
   };
 
   return (
-    <Formik
-      initialValues={{
-        fullName: "",
-        email: "",
-        dateOfBirth: "",
-        source: "",
-      }}
-      onSubmit={handleSubmit}
-      validationSchema={validationSchema}
-    >
-      <Form>
-        <label>
-          Full name
-          <Field name="fullName" />
-          <ErrorMessage name="fullName" component="span" />
-        </label>
-
-        <label>
-          Email
-          <Field name="email" />
-          <ErrorMessage name="email" component="span" />
-        </label>
-
-        <label>
-          Date of birth
-          <Field name="dateOfBirth" />
-          <ErrorMessage name="dateOfBirth" component="span" />
-        </label>
-        <label>
-          Where did you here about this event?
+    <div>
+      <Formik
+        initialValues={{
+          fullName: "",
+          email: "",
+          dateOfBirth: "",
+          source: "",
+        }}
+        onSubmit={handleSubmit}
+        validationSchema={validationSchema}
+      >
+        <Form>
           <label>
-            Social media
-            <Field type="radio" name="source" value="social media" />
+            Full name
+            <Field name="fullName" />
+            <ErrorMessage name="fullName" component="span" />
           </label>
           <label>
-            Friends
-            <Field type="radio" name="source" value="friends" />
+            Email
+            <Field name="email" />
+            <ErrorMessage name="email" component="span" />
           </label>
           <label>
-            Found myself
-            <Field type="radio" name="source" value="found myself" />
+            Date of birth
+            <Field name="dateOfBirth" />
+            <ErrorMessage name="dateOfBirth" component="span" />
           </label>
-          <ErrorMessage name="source" component="span" />
-        </label>
-
-        <button type="submit">Register</button>
-      </Form>
-    </Formik>
+          <label>
+            Where did you here about this event?
+            <label>
+              Social media
+              <Field type="radio" name="source" value="social media" />
+            </label>
+            <label>
+              Friends
+              <Field type="radio" name="source" value="friends" />
+            </label>
+            <label>
+              Found myself
+              <Field type="radio" name="source" value="found myself" />
+            </label>
+            <ErrorMessage name="source" component="span" />
+          </label>
+          <button type="submit">Register</button>
+        </Form>
+      </Formik>
+      {loading && <p>Looking for available seats..</p>}
+    </div>
   );
 }
